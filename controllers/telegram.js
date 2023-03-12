acecc573bdb8709cc82aeb2c6c6609553ebe684d
Nur Fahmi <hello@rfahmi.com>
2023-03-12 18:58:28 +0700

    add setwebhook

--- controllers/telegram.js
+++ controllers/telegram.js
@@ -3,8 +3,10 @@ const { generateText } = require("../services/gpt");
 require("dotenv").config();
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+const BASE_URL = process.env.BASE_URL;
 
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: false });
+bot.setWebHook(`${BASE_URL}/bot`);
 
 async function sendMessage(text, chatId) {
   const generatedText = await generateText(text);
