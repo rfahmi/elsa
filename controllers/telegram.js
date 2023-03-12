132a1b866b89fe0906fe8b72fec2f158f2cfa18b
Nur Fahmi <hello@rfahmi.com>
2023-03-12 20:01:00 +0700

    Update chatgpt

--- controllers/telegram.js
+++ controllers/telegram.js
@@ -4,7 +4,7 @@ const { generateText } = require("../services/gpt");
 
 async function ask(text) {
   const generatedText = await generateText(text);
-  //   console.log(generateText);
+  console.log(generateText);
   return generatedText;
 }
 
--- services/gpt.js
+++ services/gpt.js
@@ -1,27 +1,33 @@
-const { OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const GPT_MODEL = process.env.GPT_MODEL;
 const GPT_TEMPERATURE = process.env.GPT_TEMPERATURE;
 
-const openai = new OpenAIApi(OPENAI_API_KEY, {
-  headers: {
-    Authorization: `Bearer ${OPENAI_API_KEY}`,
-  },
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
 });
 
-async function generateText(prompt) {
-  const completion = await openai.createCompletion({
-    model: GPT_MODEL,
-    prompt,
-    maxTokens: 100,
-    temperature: GPT_TEMPERATURE,
-    n: 1,
-    stop: "\n",
-  });
+const openai = new OpenAIApi(configuration);
 
-  return completion.data.choices[0].text;
+async function generateText(prompt) {
+  try {
+    const completion = await openai.createChatCompletion({
+      model: GPT_MODEL,
+      messages: [{ role: "user", content: prompt }],
+      temperature: Number(GPT_TEMPERATURE),
+    });
+
+    return completion.data.choices[0].message.content;
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
 }
 
 module.exports = { generateText };
