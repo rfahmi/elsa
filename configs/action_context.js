dec37b123148b4edc97f0aca1af909a777c4194d
Nur Fahmi <hello@rfahmi.com>
2023-03-30 22:35:27 +0700

    clear project

--- configs/action_context.js
+++
@@ -1,9 +0,0 @@
-const sentences = [
-  { id: "CODING", text: "membuat kode program dengan bahasa pemrograman" },
-  { id: "SMARTHOME_CONTROL", text: "mengontrol perangkat elektronik di rumah" },
-  {
-    id: "IMAGE_GENERATION",
-    text: "membuat atau mengenerate gambar berdasarkan deskripsi yang diberikan, tapi tidak termasuk mengidentifikasi suatu gambar",
-  },
-];
-module.exports = { sentences };
--- controllers/telegram.js
+++
@@ -1,8 +0,0 @@
-const openai = require("../services/openai");
-
-async function ask(text) {
-  const response = await openai.ask(text);
-  return response;
-}
-
-module.exports = { ask };
